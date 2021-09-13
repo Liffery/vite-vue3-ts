8b15f19d56222d4f39af54a54a33e2ad85b3d731
lianglifang <PoD2020@sics@LlF1228>
2021-09-13 19:27:57 +0800

    feat: 初始化项目

---
+++ src/views/axios.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>axios</h1>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from "../utils/axios";
+
+export default defineComponent({
+  setup() {
+    axios
+      .get("/users/XPoet")
+      .then((res) => {
+        console.log("res: ", res);
+      })
+      .catch((err) => {
+        console.log("err: ", err);
+      });
+  },
+});
+</script>
---
+++ src/views/vuex.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>vuex</h1>
+</template>
